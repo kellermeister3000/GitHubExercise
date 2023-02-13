8209f8387672b4cf9642c4426858ef55e7980533
Philip Keller <philipjoelkeller@gmail.com>
2023-02-13 12:35:55 -0800

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Philip Keller on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
